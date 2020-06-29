5b970c5b901b9640fb2f49dc73ca401f6d9507a7
AyeIIInuk <66727863+AyeIIInuk@users.noreply.github.com>
2020-06-29 21:25:04 +0300

    123

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,10 +1,8 @@
 import os.path
-import subprocess
 
-try:
-  VERSION = subprocess.check_output(["git", "describe", "--tags", "--always"]).decode('ascii').strip()
-except Exception:
-  VERSION = 'version_unknown'
+MAIN_VERSION = '1.9.5_2'
+SUB_VERSION = ''
+VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
 DISCORD_MSG_CHAR_LIMIT = 2000
